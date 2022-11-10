d0bf1a07e1eac6b944812279f8ac7f40300fcb75
Kristen Schau <47155823+krschau@users.noreply.github.com>
2022-11-10 15:33:44 -0800

    Copy uwp files and MainWindow contents

--- source/winui3/AdaptiveCardVisualizer/MainWindow.xaml.cs
+++ source/winui3/AdaptiveCardVisualizer/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.UI.Xaml;
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Controls.Primitives;
 using Microsoft.UI.Xaml.Data;
@@ -13,6 +15,9 @@ using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 
+using AdaptiveCards.Rendering.Uwp;
+using AdaptiveCardVisualizer.ViewModel;
+
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
 
@@ -26,11 +31,97 @@ namespace AdaptiveCardVisualizer
         public MainWindow()
         {
             this.InitializeComponent();
+
+            Load();
         }
 
-        private void myButton_Click(object sender, RoutedEventArgs e)
+        private async void Load()
         {
-            myButton.Content = "Clicked";
+            IsEnabled = false;
+
+            ViewModel = await MainPageViewModel.LoadAsync();
+            DataContext = ViewModel;
+
+            IsEnabled = true;
+        }
+
+        private void loadFileButton_Clicked(object sender, RoutedEventArgs args)
+        {
+            ViewModel.OpenDocument();
+        }
+
+        private void ButtonNewCard_Click(object sender, RoutedEventArgs e)
+        {
+            ViewModel.NewDocument();
+        }
+
+        private void AppBarNew_Click(object sender, RoutedEventArgs e)
+        {
+            ViewModel.NewDocument();
+        }
+
+        private void AppBarOpen_Click(object sender, RoutedEventArgs e)
+        {
+            ViewModel.OpenDocument();
+        }
+
+        private async void AppBarSave_Click(object sender, RoutedEventArgs e)
+        {
+            if (ViewModel.CurrentDocument == null)
+            {
+                return;
+            }
+
+            IsEnabled = false;
+            await ViewModel.CurrentDocument.SaveAsync();
+            IsEnabled = true;
+        }
+
+        private void CommandBar_Opening(object sender, object e)
+        {
+            SetIsCompactOnAppBarButtons(false);
+        }
+
+        private void CommandBar_Closing(object sender, object e)
+        {
+            SetIsCompactOnAppBarButtons(true);
+        }
+
+        private void SetIsCompactOnAppBarButtons(bool isCompact)
+        {
+            foreach (var button in StackPanelMainAppBarButtons.Children.OfType<ICommandBarElement>())
+            {
+                button.IsCompact = isCompact;
+            }
+        }
+
+        private void AppBarHostConfigEditor_Click(object sender, RoutedEventArgs e)
+        {
+            SetIsInHostConfigEditor(!IsInHostConfigEditor);
+        }
+
+        public bool IsInHostConfigEditor { get; private set; }
+
+        private void SetIsInHostConfigEditor(bool isInHostConfigEditor)
+        {
+            IsInHostConfigEditor = isInHostConfigEditor;
+
+            AdaptiveCardDocumentView.IsEnabled = !isInHostConfigEditor;
+
+            foreach (var button in StackPanelMainAppBarButtons.Children.OfType<ButtonBase>())
+            {
+                if (button != AppBarHostConfigEditor)
+                {
+                    button.IsEnabled = !isInHostConfigEditor;
+                }
+            }
+
+            HostConfigEditorView.Visibility = isInHostConfigEditor ? Visibility.Visible : Visibility.Collapsed;
+        }
+
+        private void HostConfigTransparentBackdrop_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            SetIsInHostConfigEditor(false);
         }
     }
 }
